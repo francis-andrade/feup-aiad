04da37b65708abdd332a99d4bb2e531b864148ff
francis-andrade <up201503481@fe.up.pt>
2018-11-08 23:40:52 +0000

    added new model

--- src/models/Launcher.java
+++ src/models/Launcher.java
@@ -27,7 +27,7 @@ public class Launcher {
 	public static void main(String[] args) {
 		setUpJADE();
 		AgentsWindow.launch();
-		runModel2();
+		runModel3();
 		launchAgents();
 	}
 	
@@ -52,7 +52,7 @@ public class Launcher {
 		System.out.print("runtime created\n");
 
 		// Create a default profile
-		Profile profile = new ProfileImpl(null, 1203, null);
+		Profile profile = new ProfileImpl(null, 1200, null);
 		System.out.print("profile created\n");
 
 		System.out.println("Launching a whole in-process platform..."+profile);
@@ -118,6 +118,40 @@ public class Launcher {
 		
 		ArrayList<Integer> coordinatess2 = new ArrayList<Integer>(Arrays.asList(20, 20));
 		CivilProtectionAgent station2 = new CivilProtectionAgent(coordinatess2, 2, 2, 2, 2);
+		
+		
+		ArrayList<CivilProtectionAgent> civilProtectionList = new ArrayList<CivilProtectionAgent>(Arrays.asList(station1, station2));
+		DispatcherAgent dispatcher = new DispatcherAgent(civilProtectionList);
+		
+		station1.setCivilProtectionStations(civilProtectionList);
+		station2.setCivilProtectionStations(civilProtectionList);
+		
+		Launcher.citizens = new ArrayList<CitizenAgent>(Arrays.asList(citizen1, citizen2, citizen3));
+		Launcher.stations = civilProtectionList;
+		Launcher.dispatcher = dispatcher;
+	}
+	
+	public static void runModel3() {
+		ArrayList<Integer> coordinatesc1 = new ArrayList<Integer>(Arrays.asList(1, 1));
+		ArrayList<Emergency> emergencies1 = new ArrayList<Emergency>();
+		emergencies1.add(EmergencyList.getEmergencies().get(3));
+		CitizenAgent citizen1 = new CitizenAgent(coordinatesc1, emergencies1, 1, 3, 1);
+		
+		ArrayList<Integer> coordinatesc2 = new ArrayList<Integer>(Arrays.asList(2, 2));
+		ArrayList<Emergency> emergencies2 = new ArrayList<Emergency>();
+		emergencies2.add(EmergencyList.getEmergencies().get(15));
+		CitizenAgent citizen2 = new CitizenAgent(coordinatesc2, emergencies2, 1, 4, 2);
+		
+		ArrayList<Integer> coordinatesc3 = new ArrayList<Integer>(Arrays.asList(3, 3));
+		ArrayList<Emergency> emergencies3 = new ArrayList<Emergency>();
+		emergencies3.add(EmergencyList.getEmergencies().get(3));
+		CitizenAgent citizen3 = new CitizenAgent(coordinatesc3, emergencies3, 1, 5, 3);
+		
+		ArrayList<Integer> coordinatess1 = new ArrayList<Integer>(Arrays.asList(0, 0));
+		CivilProtectionAgent station1 = new CivilProtectionAgent(coordinatess1, 1, 1, 1, 1);
+		
+		ArrayList<Integer> coordinatess2 = new ArrayList<Integer>(Arrays.asList(20, 20));
+		CivilProtectionAgent station2 = new CivilProtectionAgent(coordinatess2, 2, 1, 1, 1);
 		
 		
 		ArrayList<CivilProtectionAgent> civilProtectionList = new ArrayList<CivilProtectionAgent>(Arrays.asList(station1, station2));
